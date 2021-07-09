67fb62130b897ed049e2991eefdaf3e4dc6877d6
Pawel Konior <pawel.konior@gmail.com>
2021-07-09 19:37:41 +0200

    default cotrib.auth

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,9 @@ STATICFILES_FINDERS = [
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')),
+
     # Local apps
     path('', include('home.urls'))
 ]
